e8609556f5185fa084fe363dac264e3e75137238
Jaime Cardoso <70758067+Jaime-Cardoso@users.noreply.github.com>
2023-05-11 22:55:49 -0300

    Create method implemented

---
+++ src/main/java/br/com/devFuture/Cliente/service/ClientService.java
@@ -0,0 +1,8 @@
+package br.com.devFuture.Cliente.service;
+
+import br.com.devFuture.Cliente.entities.Client;
+
+public interface ClientService {
+
+    Client create(Client client);
+}
---
+++ src/main/java/br/com/devFuture/Cliente/service/impl/ClientServiceImpl.java
@@ -0,0 +1,19 @@
+package br.com.devFuture.Cliente.service.impl;
+
+import br.com.devFuture.Cliente.entities.Client;
+import br.com.devFuture.Cliente.repository.ClientRepository;
+import br.com.devFuture.Cliente.service.ClientService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class ClientServiceImpl implements ClientService {
+
+    private final ClientRepository clientRepository;
+    @Override
+    public Client create(Client client) {
+        return clientRepository.save(client);
+    }
+}
